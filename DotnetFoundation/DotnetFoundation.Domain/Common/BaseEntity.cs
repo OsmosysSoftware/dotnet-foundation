95be3cc3b05ff67ad27fdcf64a22dfe0880d77a9
Shreyansh Jain <shreyansh.j@osmosys.co>
2024-02-23 16:16:34 +0530

    Update field datatype for base entity

--- DotnetFoundation/DotnetFoundation.Domain/Common/BaseEntity.cs
+++ DotnetFoundation/DotnetFoundation.Domain/Common/BaseEntity.cs
@@ -4,9 +4,9 @@ namespace DotnetFoundation.Domain.Common;
 public abstract class BaseEntity
 {
     public int Id { get; set; }
-    public int CreatedOn { get; set; }
+    public DateTime CreatedOn { get; set; }
     public int CreatedBy { get; set; }
-    public int ModifiedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
     public int ModifiedBy { get; set; }
     public Status Status { get; set; }
 }
