37b97aef120e8a0f5dacec873f3e3c80a37cd35f
Shreyansh Jain <shreyansh.j@osmosys.co>
2024-02-23 17:51:46 +0530

    Add regex model validator for password

--- DotnetFoundation/DotnetFoundation.Application/Models/DTOs/AuthenticationDTO/LoginRequest.cs
+++ DotnetFoundation/DotnetFoundation.Application/Models/DTOs/AuthenticationDTO/LoginRequest.cs
@@ -9,5 +9,7 @@ public record LoginRequest
     public string Email { get; init; }
 
     [Required(ErrorMessage = "Password is required")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$",
+        ErrorMessage = "8-10 character length with 1 uppercase, 1 lowercase, 1 number, 1 special character is required")]
     public string Password { get; init; }
 }
