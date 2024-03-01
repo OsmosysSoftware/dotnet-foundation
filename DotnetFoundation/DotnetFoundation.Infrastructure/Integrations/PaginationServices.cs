195062b48099005a9000e3c4601b0d59d4c7d156
kshitij.k <kshitij.k@osmosys.co>
2024-03-01 18:53:15 +0530

    docs: added comment

--- DotnetFoundation/DotnetFoundation.Infrastructure/Integrations/PaginationServices.cs
+++ DotnetFoundation/DotnetFoundation.Infrastructure/Integrations/PaginationServices.cs
@@ -7,6 +7,7 @@ public class PagiationServices<T> : IPagiationServices<T>
 {
     public async Task<PagedList<T>> ToPagedListAsync(IQueryable<T> query, int pageNumber, int pageSize)
     {
+        // Below assigning allows to fetch atleast one value, if exists, when no parameters are passed
         if (pageNumber <= 1) { pageNumber = 1; }
         if (pageSize <= 1) { pageSize = 1; }
         int totalCount = await query.CountAsync().ConfigureAwait(false);
