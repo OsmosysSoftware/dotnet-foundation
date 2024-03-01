b63fde450dd727038281399cb2915c4f9fd322ba
kshitij.k <kshitij.k@osmosys.co>
2024-03-01 15:04:04 +0530

    fix: limit for budgeted hours

--- DotnetFoundation/DotnetFoundation.Application/Models/DTOs/TaskDetailsDTO/TaskDetailsRequest.cs
+++ DotnetFoundation/DotnetFoundation.Application/Models/DTOs/TaskDetailsDTO/TaskDetailsRequest.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
 
 namespace DotnetFoundation.Application.Models.DTOs.TaskDetailsDTO;
 
@@ -14,7 +15,7 @@ public record TaskDetailsRequest
     /// Gets the Budgeted Hours of the task.
     /// </summary>
     [Required(ErrorMessage = "BudgetedHours is required")]
-    [Range(0, int.MaxValue, ErrorMessage = "BudgetedHours should be in range of 0 to int.Maxvalue")]
+    [Range(0, 50, ErrorMessage = "BudgetedHours should be in range of 0 to 50")]
     public int BudgetedHours { get; init; }
     /// <summary>
     /// Gets the user Id of the user the task is assigned to.
