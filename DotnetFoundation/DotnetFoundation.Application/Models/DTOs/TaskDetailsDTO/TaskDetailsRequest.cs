4a8f892960b295c3396784491672084ac6060cf7
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2024-03-12 16:34:50 +0530

    feat: add generic pagination (#27)

--- DotnetFoundation/DotnetFoundation.Application/Models/DTOs/TaskDetailsDTO/TaskDetailsRequest.cs
+++ DotnetFoundation/DotnetFoundation.Application/Models/DTOs/TaskDetailsDTO/TaskDetailsRequest.cs
@@ -8,12 +8,13 @@ public record TaskDetailsRequest
     /// Gets the Description of the task.
     /// </summary>
     [Required(ErrorMessage = "Description is required")]
+    [StringLength(100, ErrorMessage = "Description Max Length is 100")]
     public string Description { get; init; } = string.Empty;
     /// <summary>
     /// Gets the Budgeted Hours of the task.
     /// </summary>
     [Required(ErrorMessage = "BudgetedHours is required")]
-    [Range(0, int.MaxValue, ErrorMessage = "BudgetedHours should be in range of 0 to int.Maxvalue")]
+    [Range(0, 50, ErrorMessage = "BudgetedHours should be in range of 0 to 50")]
     public int BudgetedHours { get; init; }
     /// <summary>
     /// Gets the user Id of the user the task is assigned to.
@@ -23,5 +24,6 @@ public record TaskDetailsRequest
     /// <summary>
     /// Gets the Category of the task.
     /// </summary>
+    [StringLength(100, ErrorMessage = "Category Max Length is 100")]
     public string? Category { get; init; }
 }
