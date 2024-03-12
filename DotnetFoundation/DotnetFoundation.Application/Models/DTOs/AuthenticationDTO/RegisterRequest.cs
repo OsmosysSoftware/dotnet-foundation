1c6a8281edcaf6793acf27b85550ebbbc61db39d
harish <harish@osmosys.co>
2024-03-12 12:37:04 +0530

    fix: update registerRequest dto

--- DotnetFoundation/DotnetFoundation.Application/Models/DTOs/AuthenticationDTO/RegisterRequest.cs
+++ DotnetFoundation/DotnetFoundation.Application/Models/DTOs/AuthenticationDTO/RegisterRequest.cs
@@ -3,7 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace DotnetFoundation.Application.Models.DTOs.AuthenticationDTO;
 public record RegisterRequest
 {
+    [Required(ErrorMessage = "First Name is Required")]
     public string FirstName { get; init; } = String.Empty;
+    [Required(ErrorMessage = "Last Name is Required")]
     public string LastName { get; init; } = String.Empty;
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Invalid email")]
@@ -12,7 +14,8 @@ public record RegisterRequest
     [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$",
         ErrorMessage = "8-10 character length with 1 uppercase, 1 lowercase, 1 number, 1 special character is required")]
     public string Password { get; init; } = String.Empty;
-    public string? Country { get; init; }
+    [Required(ErrorMessage = "Country is Required")]
+    public string Country { get; init; } = string.Empty;
     [Phone]
     public string? PhoneNumber { get; init; }
 }
